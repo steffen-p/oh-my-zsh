201922e1fcd858d87fa378287f07524d5b4018e8
Steffen Pengel <spengel@de.adit-jv.com>
2017-01-17 14:45:24 +0100

    custom: plugin: add puregit
    Add pure git-prompt.sh from git repo.
    
    Signed-off-by: Steffen Pengel <spengel@de.adit-jv.com>

---
+++ custom/plugins/gitpure/gitpure.plugin.zsh
@@ -0,0 +1,9 @@
+dir=$(dirname $0)
+
+export GIT_PS1_SHOWSTASHSTATE=1
+export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWUNTRACKEDFILES=1
+export GIT_PS1_SHOWUPSTREAM="auto verbose name"
+export GIT_PS1_DESCRIBE_STYLE="branch"
+
+source $dir/git-prompt.sh
