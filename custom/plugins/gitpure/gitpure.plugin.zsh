cb8cc1e47ac6eff3099057381657cd25f565af93
Steffen Pengel <spengel@de.adit-jv.com>
2017-01-19 10:10:24 +0100

    custom: plugin: puregit: activate colored git ps1 extension
    Signed-off-by: Steffen Pengel <spengel@de.adit-jv.com>

--- custom/plugins/gitpure/gitpure.plugin.zsh
+++ custom/plugins/gitpure/gitpure.plugin.zsh
@@ -2,6 +2,7 @@ dir=$(dirname $0)
 
 export GIT_PS1_SHOWSTASHSTATE=1
 export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWCOLORHINTS=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
 export GIT_PS1_SHOWUPSTREAM="auto verbose name"
 export GIT_PS1_DESCRIBE_STYLE="branch"
