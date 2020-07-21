bdc811316ed3758454bee5cb0c93aaf6ca844681
Steffen Pengel <spengel@de.adit-jv.com>
2020-07-21 11:27:04 +0900

    custom: plugin: puregit: change config
    Signed-off-by: Steffen Pengel <spengel@de.adit-jv.com>

--- custom/plugins/gitpure/gitpure.plugin.zsh
+++ custom/plugins/gitpure/gitpure.plugin.zsh
@@ -1,10 +1,10 @@
 dir=$(dirname $0)
 
 export GIT_PS1_SHOWSTASHSTATE=1
-export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWDIRTYSTATE=""
 export GIT_PS1_SHOWCOLORHINTS=1
-export GIT_PS1_SHOWUNTRACKEDFILES=1
-export GIT_PS1_SHOWUPSTREAM="auto verbose name"
+export GIT_PS1_SHOWUNTRACKEDFILES=""
+export GIT_PS1_SHOWUPSTREAM="auto name"
 export GIT_PS1_DESCRIBE_STYLE="branch"
 
 source $dir/git-prompt.sh
